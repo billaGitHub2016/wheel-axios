6513bb321d3af6b9a0a4f959683fb0dbcd204a6e
billa <billa2013@163.com>
2019-06-05 21:52:48 +0800

    test(app.ts): 添加post请求body数据测试
    添加post请求body数据测试

--- examples/simple/app.ts
+++ examples/simple/app.ts
@@ -1,10 +1,67 @@
 import axios from '../../src/index'
 
 axios({
-  method: 'get',
-  url: '/simple/get',
-  params: {
-    a: 1,
-    b: 2
-  }
+    method: 'get',
+    url: '/simple/get',
+    params: {
+        a: 1,
+        b: 2
+    }
+})
+
+// params是数组
+axios({
+    method: 'get',
+    url: '/base/get',
+    params: {
+        a: [1, 2, 3],
+        b: 2
+    }
+})
+
+// params是日期
+axios({
+    method: 'get',
+    url: '/base/get',
+    params: {
+        a: new Date(),
+        b: 2
+    }
+})
+
+// params是对象
+axios({
+    method: 'get',
+    url: '/base/get',
+    params: {
+        a: {
+            c: 1,
+            d: 2,
+        },
+        b: 2
+    }
+})
+
+// params是特殊字符
+axios({
+    method: 'get',
+    url: '/base/get',
+    params: {
+        a: '@:$',
+        b: 2
+    }
+})
+
+axios({
+    method: 'post',
+    url: '/base/post',
+    data: {
+        a: 1,
+        b: 2,
+    }
+})
+axios({
+    method: 'post',
+    url: '/base/buffer',
+    data: new Int32Array([21, 31])
 })
