457e19a8b3200ba2871b3d2dc43d479a3207da3f
billa <billa2013@163.com>
2019-06-05 21:56:23 +0800

    feat(src/index.ts): 添加请求data处理逻辑
    添加请求data处理逻辑

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { AxiosRequestConfig } from './types'
 import xhr from './xhr'
 import { bindUrl } from './helpers/url'
+import { transfromRequest } from './helpers/data'
 
 function axios(config: AxiosRequestConfig) {
   processConfig(config)
@@ -9,11 +10,16 @@ function axios(config: AxiosRequestConfig) {
 
 function processConfig(config: AxiosRequestConfig): void {
   config.url = transformUrl(config)
+  config.data = transformRequestData(config)
 }
 
 function transformUrl(config: AxiosRequestConfig): string {
   const { url, params } = config
   return bindUrl(url, params)
+}
+
+function transformRequestData(config: AxiosRequestConfig): any {
+  return transfromRequest(config.data)
 }
 
 export default axios
