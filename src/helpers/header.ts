b1bd5dc60b09e130f4112a25e78a86e4226e7e8b
billa <billa2013@163.com>
2019-06-20 19:35:26 +0800

    feat: 添加 回复header字符串转换为对象的特性

--- src/helpers/header.ts
+++ src/helpers/header.ts
@@ -21,3 +21,38 @@ export function processHeaders(headers: any, data: any): any {
 
   return headers
 }
+
+/**
+ * 把服务器返回的headers字符串转换为对象
+ * @param headers 服务器返回的headers字符串
+ * headers e.g.
+ * 'date: Fri, 05 Apr 2019 12:40:49 GMT\r\n
+    etag: W/"d-Ssxx4FRxEutDLwo2+xkkxKc4y0k"\r\n
+    connection: keep-alive\r\n
+    x-powered-by: Express\r\n
+    content-length: 13\r\n
+    content-type: application/json; charset=utf-8'
+ */
+export function parseHeaders(headers: string): any {
+  const parsed = Object.create(null)
+
+  if (!headers) {
+    return
+  }
+
+  headers.split('\r\n').forEach(header => {
+    let [key, ...val] = header.split(':')
+    key = key.trim().toLowerCase()
+
+    if (!key) {
+      return
+    }
+    let value = undefined
+    if (val) {
+      value = val.join(':').trim()
+    }
+    parsed[key] = value
+  })
+
+  return parsed
+}
